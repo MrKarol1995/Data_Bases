4a1ddad265ef6d5bf4bda14bc0adda89b4e8a66a
Karol Cieślik <karolcieslik121@wp.pl>
2025-04-04 11:57:27 +0200

    Update L5.sql

--- L5.sql
+++ L5.sql
@@ -34,3 +34,21 @@ SELECT country, ROUND(sum(right(CustomerID,1)='A') * 100.0 / count(*), 2) as pro
 FROM northwind.Customers
 GROUP BY country
 ORDER BY procent DESC LIMIT 5;
+
+--5 bis 
+--5.2
+SELECT CustomerID, COUNT(*) AS liczba_zamowien
+FROM Orders
+GROUP BY CustomerID
+HAVING liczba_zamowien > 20; 
+
+--5.3
+SELECT CONCAT(ROUND(SUM(Discontinued = "01") / COUNT(*) * 100, 0), "%")
+FROM Products;
+
+--5.4
+SELECT Country, SUM(RIGHT(LEFT(ContactName, LOCATE(' ', ContactName) - 1), 1) = 'a') / COUNT(*) AS odsetek
+FROM Customers
+GROUP BY Country
+ORDER BY odsetek DESC, Country
+LIMIT 5;
